15548294fe53c1dc78d3b29b17420e0b1fb3ea06
kktech <kktech@kktechs-MacBook-Air.local>
2026-04-23 04:11:24 +0630

    Fix white screen by adding base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/outline-vpn-guide/',
   build: {
     minify: 'terser',
     terserOptions: {
